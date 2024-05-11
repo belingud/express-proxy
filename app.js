c1fe6f0814ed3d5ecacec074470677c571ef39fb
belingud <1170202353@qq.com>
2024-05-11 21:03:34 +0800

    use http-proxy-middleware

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 let express = require("express");
 let morgan = require("morgan");
-let axios = require("axios");
-const https = require("https");
+const {
+    createProxyMiddleware,
+    debugProxyErrorsPlugin, // subscribe to proxy errors to prevent server from crashing
+    errorResponsePlugin, // return 5xx response on proxy error
+    proxyEventsPlugin, // implements the "on:" option
+    loggerPlugin,
+} = require("http-proxy-middleware");
+
+let STATUS_CODES = require("./statuscode");
+
 // disable certificate verification
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = false;
 
 const winston = require("winston");
 const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
-  format: winston.format.combine(
-    winston.format.timestamp(),
-  ),
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(winston.format.timestamp()),
 });
 logger.info(`Runing environment: ${process.env.NODE_ENV}`);
+const NO_BODY_METHODS = ["GET", "HEAD"];
+const TARGET_PARAM_NAME = "target";
 
 const app = express();
 
@@ -20,68 +28,135 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+// 允许跨域
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 app.options("*", (req, res) => {
-  res.sendStatus(200);
+    res.sendStatus(HTTP_STATUS.OK);
 });
 
 app.get("/hello", (req, res) => {
-  logger.info("roota is: ", req.url);
-  res.send("Hello World!");
-})
+    logger.info("In HELLO: ", req.url);
+    res.send("Hello World!");
+});
 
-app.all("/", async (req, res) => {
-  const url = req.query.url;
-  logger.info("param url is: ", url);
-  if (!url) {
-    res.status(400).send("NOT IN SERVICE");
-    return;
-  }
+// 解析请求参数为URL对象
+function parseUrlParam(req) {
+    const proxyUrlWithQuery = req.query[TARGET_PARAM_NAME]; // 从query string获取url参数
+    // 尝试解析URL及其查询参数
+    try {
+        const url = new URL(proxyUrlWithQuery);
+        // 这里可以添加额外的验证逻辑，例如检查协议或主机等
+        return url;
+    } catch (err) {
+        console.error("Invalid URL parameter:", err);
+        return null;
+    }
+}
 
-  try {
-    let response;
-    const instance = axios.create({
-      httpsAgent: new https.Agent({
-        rejectUnauthorized: false,
-      }),
+app.all("/", (req, res) => {
+    const proxyUrl = parseUrlParam(req);
+    console.log("in proxy api: ", proxyUrl);
+    req.url = proxyUrl.pathname + proxyUrl.search;
+    console.log("new req.url is: ", req.url);
+
+    // 如果URL参数无效，返回错误响应
+    if (!proxyUrl) {
+        return res.status(STATUS_CODES.BAD_REQUEST).send("ILLEGAL PARAMS");
+    }
+    // 协议校验
+    if (proxyUrl.protocol !== "http:" && proxyUrl.protocol !== "https:") {
+        return res
+            .status(STATUS_CODES.BAD_REQUEST)
+            .send("Only HTTP and HTTPS protocols are allowed");
+    }
+
+    // 动态创建代理中间件
+    const proxy = createProxyMiddleware({
+        // logger: console,
+        target: proxyUrl.origin, // 使用带protocal的地址
+        changeOrigin: true,
+        plugins: [
+            debugProxyErrorsPlugin,
+            loggerPlugin,
+            errorResponsePlugin,
+            proxyEventsPlugin,
+        ],
     });
 
-    if (req.method === "GET") {
-      response = await instance.get(url, { headers: req.headers });
-    } else if (req.method === "POST") {
-      response = await instance.post(url, req.body, { headers: req.headers });
-    } else if (req.method === "PUT") {
-      response = await instance.put(url, req.body, { headers: req.headers });
-    } else if (req.method === "DELETE") {
-      response = await instance.delete(url, { headers: req.headers });
+    // 使用代理中间件
+    proxy(req, res);
+});
+
+app.all("/proxy", async (request, response, next) => {
+    // 从请求的查询参数中获取目标视频URL
+    const url = request.query.url;
+    if (!url) {
+        return response.status(STATUS_CODES.BAD_REQUEST).send("NOT IN SERVICE");
     }
-    
-    res.send(response.data);
-  } catch (error) {
-    logger.error(error);
-    res.status(500).send(error.stack);
-  }
+    console.log(request.url);
+    console.log(request.headers);
+    let targetUrl;
+    try {
+        targetUrl = new URL(url);
+    } catch (error) {
+        console.error(error);
+        return response.status(STATUS_CODES.BAD_REQUEST).send("ILLEGAL PARAMS");
+    }
+
+    console.log("start to proxy to target url: ", targetUrl);
+    // 发起请求到目标视频服务器
+    let requestInit = {
+        method: request.method,
+        headers: request.headers, // 将请求头原样传递给视频服务器
+    };
+    if (!NO_BODY_METHODS.includes(request.method)) {
+        requestInit.body = request.body;
+    }
+    let proxyRes;
+    try {
+        proxyRes = await fetch(targetUrl, requestInit);
+    } catch (error) {
+        return next(error);
+    }
+    console.log("response status: ", proxyRes.status);
+    console.log(proxyRes.headers.get("location"));
+    console.log(proxyRes.headers.get("Content-Type"));
+    return response
+        .status(proxyRes.status)
+        .setHeaders(proxyRes.headers)
+        .send(proxyRes.body);
 });
 
 // app.listen(3000, () => {
 //   console.log("Proxy server is running on port 3000");
 // });
 
+// 异常中间件
+app.use((err, req, res, next) => {
+    // 记录错误
+    console.error(err.stack);
+    // 错误响应
+    res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({ error: err.message });
+});
+
 module.exports = app;
 
 if (process.env.NODE_ENV === "development") {
-  app.listen(process.env.PORT || 3000, async () => {
-    console.log("Server started on port " + (process.env.PORT || 3000));
-  });
+    app.listen(process.env.PORT || 3000, async () => {
+        console.log("Server started on port " + (process.env.PORT || 3000));
+    });
 }
